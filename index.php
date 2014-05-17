569fcd6388348b1e6394ea5b9569f6f09765e5a4
Jewel Mahmud <jewelmahmudsinfo@gmail.com>
2014-05-17 15:17:30 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>This is a simple love clculator</title>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+		<link href='http://fonts.googleapis.com/css?family=Fascinate+Inline' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Allan' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div class="main fix">
+			<h2 id="id">Calculate your Love Using this App</h2>
+			<div class="form_area fix">
+				<form action="lovecalc.php" method="POST">
+				<input type="text" name="name_one" placeholder="Your Name"><br>
+				<input type="text" name="name_two" placeholder="Your Love Name">
+				<input type="submit" value="Calculate"><br><br>
+			</div>
+			<div class="result_area">
+			<?php 
+				$string_one = $_POST["name_one"]; 
+				$string_two = $_POST["name_two"];
+				similar_text($string_one, $string_two, $result);
+				echo "You Two Love each other in Percentage: <br><br><span>".$result."%</span>";
+			?>
+			</div>
+		</div>
+
+	</body>
+</html>
